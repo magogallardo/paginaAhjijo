193b13338bea9eda2e77bcf34f5d673de22a781d
agallardodom <deg4_gow@hotmail.com>
2020-12-29 22:12:34 -0600

    Deploy try 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Esto se convertirá en el sistema de venta AhJijo" />
+    <img alt="Ahjijo logo" src="../assets/logo.png" />
   </div>
 </template>
 
